143cddc8199e41437e711317d9f42cb7a4617c3e
Valentina <valentina.cecchini.94@gmail.com>
2019-02-01 18:06:33 +0100

    added addMovie service in vault

--- code/vault/src/main/java/it/univaq/disim/netflics/vault/service/VaultServiceImpl.java
+++ code/vault/src/main/java/it/univaq/disim/netflics/vault/service/VaultServiceImpl.java
@@ -1,27 +1,37 @@
 package it.univaq.disim.netflics.vault.service;
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.Arrays;
 
+import com.google.gson.JsonObject;
 import it.univaq.disim.netflics.vault.*;
+import it.univaq.disim.netflics.vault.model.Movie;
+import it.univaq.disim.netflics.vault.repository.MovieRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import com.google.gson.Gson;
 
+import javax.activation.DataHandler;
+
 @Service
 public class VaultServiceImpl implements VaultService {
 
-	private static Logger logger = LoggerFactory.getLogger(VaultServiceImpl.class);
+	@Autowired
+	private MovieRepository movieRepository;
 
+	private static Logger LOGGER = LoggerFactory.getLogger(VaultServiceImpl.class);
 
 	@Value("#{cfg.omdburl}")
 	private String omdburl;
+
+	@Value("#{cfg.videopath}")
+	private String videopath;
 
 	@Override
 	public GetMovieResponse getMovie(GetMovieRequest parameters) throws BusinessException {
@@ -30,18 +40,26 @@ public class VaultServiceImpl implements VaultService {
 
 	@Override
 	public AddMovieResponse addMovie(AddMovieRequest parameters) throws BusinessException {
-		return new AddMovieResponse();
-	}
 
+		String title = null;
+		String directors = null;
+		String genres = null;
+		Double rating = null;
 
-/*
-	@Override
-	public TrafficResponse getTrafficInfo(TrafficRequest parameters) throws BusinessException {
+		// save file data in /resources
+		DataHandler movieData = parameters.getMovie();
+		String imdbId = parameters.getImdbId();
+
+		saveVideo(movieData, new File(videopath+imdbId));
+		LOGGER.info("movie saved.");
+
+		// omdb request (for movie metadata)
 		try {
-			// build the url
-			String url = trafficInformationurl + "?urLat=" + parameters.getUrLat() + "&urLon=" + parameters.getUrLon() + "&llLat=" + parameters.getLlLat() + "&llLon=" + parameters.getLlLon() + "&getId=" + parameters.isGetId() + "&getDist=" + parameters.isGetDist() + "&dt=" + parameters.getDt() + "&cenLat="
-					+ parameters.getCenLat() + "&cenLon=" + parameters.getCenLon() + "&maxResults=" + parameters.getMaxResults() + "&s" + parameters.getS();
+			LOGGER.info("REST request to OMDB.");
 
+			// build the url
+
+			String url = omdburl+imdbId;
 			// connect to url
 			HttpURLConnection c = null;
 
@@ -55,37 +73,76 @@ public class VaultServiceImpl implements VaultService {
 			int status = c.getResponseCode();
 
 			switch (status) {
-			case 200:
-			case 201:
-				BufferedReader br = new BufferedReader(new InputStreamReader(c.getInputStream(), "UTF-8"));
-				StringBuilder sb = new StringBuilder();
-				String line;
-				while ((line = br.readLine()) != null) {
-					sb.append(line + "\n");
-				}
-				br.close();
+				case 200:
+				case 201:
+					BufferedReader br = new BufferedReader(new InputStreamReader(c.getInputStream(), "UTF-8"));
+					StringBuilder sb = new StringBuilder();
+					String line;
+					while ((line = br.readLine()) != null) {
+						sb.append(line + "\n");
+					}
+					br.close();
 
-				if (c != null) {
-					c.disconnect();
-				}
+					if (c != null) {
+						c.disconnect();
+					}
 
-				// map the json response to java classes
-				TrafficMessageType[] messages = new Gson().fromJson(sb.toString(), TrafficMessageType[].class);
-				TrafficResponse resp = new TrafficResponse();
-				resp.getTrafficMessage().addAll(Arrays.asList(messages));
-				return resp;
+					JsonObject jobj = new Gson().fromJson(sb.toString(), JsonObject.class);
+					title = jobj.get("Title").getAsString();
+					directors = jobj.get("Director").getAsString();
+					genres = jobj.get("Genre").getAsString();
+					rating = jobj.get("imdbRating").getAsDouble();
 
-			default:
-				if (c != null) {
-					c.disconnect();
-				}
-				throw new Exception("Http Error: " + status);
+					LOGGER.info("movie data retrieved: {} {} {} {}", title, directors, genres, rating);
+
+					break;
+
+				default:
+					if (c != null) {
+						c.disconnect();
+					}
+					throw new Exception("Http Error: " + status);
 			}
 		} catch (Exception  e) {
-			logger.error("error", e);
+			LOGGER.error("error", e);
+			throw new BusinessException(e);
+		}
+
+		// movie object instantiation
+		Movie m = new Movie();
+		m.setTitle(title);
+		m.setDirectors(directors);
+		m.setGenres(genres);
+		m.setRating(rating);
+		m.setImdbId(imdbId);
+
+		// insertion to the db
+		movieRepository.save(m);
+
+		// return response
+		AddMovieResponse addMovieResponse = new AddMovieResponse();
+		addMovieResponse.setResult("success");
+
+		LOGGER.info("movie added successfully");
+
+		return addMovieResponse;
+	}
+
+	/**
+	 * utility function that is used to save
+	 * @param dataHandler container that contains the data
+	 * @param filePath where to save the file
+	 */
+	private static void saveVideo(final DataHandler dataHandler, final File filePath) {
+		filePath.getParentFile().mkdirs();
+		try{
+			FileOutputStream fileOutputStream = new FileOutputStream(filePath);
+			dataHandler.writeTo(fileOutputStream);
+			fileOutputStream.flush();
+		}catch (IOException e){
+			e.printStackTrace();
 			throw new BusinessException(e);
 		}
 	}
-	*/
 
 }
