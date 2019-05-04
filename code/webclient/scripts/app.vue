35254e9d1f4c2f477443a0425cd3e54dd60a2ffa
Stefano Valentini <valentini.stefano@outlook.com>
2019-05-04 19:07:03 +0200

    visual refinements to the webclint, added logIn, logOut rest calls

--- code/webclient/scripts/app.vue
+++ code/webclient/scripts/app.vue
@@ -16,27 +16,73 @@ toastr.options = {
   "hideMethod": "fadeOut"
 };
 
+var baseUrl = "http://localhost:8080/dispatcher/api/dispatcher/";
+
 var netflics = new Vue({
   el: '#wrapper',
   data: {
-    loggedIn: false,
-    token: null,
-    email: "",
-    password: "",
+    // rest request urls
+    logInUrl: baseUrl + "logIn",
+    logOutUrl: baseUrl + "logOut",
+
+    // application variables
+    user: {
+      token: null,
+      email: null,
+      password: null,
+      loggedIn: false
+    },
     lastViewed: [],
     bestOnes: [],
     mostViewed: [],
   },
   methods: {
+    /**
+     * resets user data
+     */
+    resetUser: function () {
+      this.user.token = null
+      this.user.email = null
+      this.user.password = null
+      this.user.loggedIn = false
+    },
+
+    /**
+     * requests a token, given the user's data
+     */
     logIn: function () {
-      requestBody = 'ggggg',
-        axios.post('', {
-          body: requestBody,
+
+      axios.post(this.logInUrl, { body: this.user }, { headers: { "Token": this.user.token } })
+        .then(response => {
+          data = response.data.token
+          if (data !== null && data !== '') {
+            this.user.token = data.token
+            this.user.loggedIn = true
+          } else {
+            throw "Authentication server error"
+          }
         })
-          .then(response => {/*todo*/ })
-          .catch(e => {
-            toastr["error"](e, "Something went wrong :(");
-          })
+        .catch(e => {
+          toastr["error"](e, "Something went wrong :(");
+        })
+    },
+
+    /**
+     * resets the user's data
+     */
+    logOut: function () {
+      axios.post(this.logOutUrl, {}, { headers: { "Token": this.user.token } })
+        .then(response => {
+          data = response.data
+          if (data) {
+            this.resetUser()
+          } else {
+            throw "Authentication server error"
+          }
+        })
+        .catch(e => {
+          toastr["error"](e, "Something went wrong :(");
+        })
     }
   }
 })
