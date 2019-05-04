e8f1be5becc5e3f1f64e23381df2be908de81c49
Stefano Valentini <valentini.stefano@outlook.com>
2019-05-04 14:11:59 +0200

    added first web client implementation

---
+++ code/webclient/scripts/app.vue
@@ -0,0 +1,45 @@
+toastr.options = {
+  "closeButton": true,
+  "debug": false,
+  "newestOnTop": true,
+  "progressBar": true,
+  "positionClass": "toast-top-right",
+  "preventDuplicates": false,
+  "onclick": null,
+  "showDuration": "1000",
+  "hideDuration": "1000",
+  "timeOut": "5000",
+  "extendedTimeOut": "2000",
+  "showEasing": "swing",
+  "hideEasing": "linear",
+  "showMethod": "fadeIn",
+  "hideMethod": "fadeOut"
+};
+
+var netflics = new Vue({
+  el: '#wrapper',
+  data: {
+    loggedIn: false,
+    token: null,
+    email: "",
+    password: "",
+    lastViewed: [],
+    bestOnes: [],
+    mostViewed: [],
+  },
+  methods: {
+    logIn: function () {
+      requestBody = 'ggggg',
+        axios.post('', {
+          body: requestBody,
+        })
+          .then(response => {/*todo*/ })
+          .catch(e => {
+            toastr["error"](e, "Something went wrong :(");
+          })
+    }
+  }
+})
+
+
+
